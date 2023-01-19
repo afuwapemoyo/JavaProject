96027632ccc88fec78d2d6483cd5645e1ed73b70
Afuwape <55412994+afuwapemoyo@users.noreply.github.com>
2023-01-18 21:20:24 -0500

    Update Main.java

--- CustomerJava/src/Presentation/Main.java
+++ CustomerJava/src/Presentation/Main.java
@@ -2,6 +2,7 @@ package Presentation;
 
 public class Main {
     public static void main(String[] args) {
+        //CALL CustomersGUI CLASS
         CustomersGUI cgui = new CustomersGUI();
     }
 }
